08a854ddedbba9b1a5e8533c88913f5f1ba59cd8
Michael Dear <michaelcdear@gmail.com>
2022-08-17 16:10:36 -0600

    commit tree 80afbfedf3055bab384407639926d9cdaad0db0e

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    string = nil
+    self.characters.count.times do |x|
+        string = "#{self.characters.find(x+1).name} - #{self.shows.find(x+1).name}"
+    end
+    string
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.find(1).full_name
+  end
+
 end
